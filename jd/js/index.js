be51a5a411881b4c37096e2792562f03ff04368f
liulaoliu <309503745@qq.com>
2019-03-02 06:36:07 +0800

    京东那个gif可能是故意设计成只播放一次的

--- jd/js/index.js
+++ jd/js/index.js
@@ -1,3 +1,8 @@
 $("i[class~='fa-window-close-o']").click(function(){
     $(".clicktoclose_banner").hide();
 })
+
+setTimeout(function(){
+    let ele1=document.getElementById('jddog');
+    ele1.src="img/jddog.png";
+}, 3000 )
