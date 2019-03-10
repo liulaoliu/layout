b4f6ab4209cd83d13c3aecf0011e5fc50c5b51ea
liulaoliu <309503745@qq.com>
2019-03-11 07:35:10 +0800

    增加了一个轮播图组件，并且把组件拖到京东布局页面，注意这个组件也是一个浮动div，轮播在div里边完成，不影响外界。js和css拖到jd文件夹之后，记得更新引入的css/js文件就可以。

---
+++ carousel component/js/test.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ * BirdWhisperer
+ * by 2gua
+ */
+var BirdWhisperer = /** @class */ (function () {
+    function BirdWhisperer(message) {
+        this.chirping = message;
+    }
+    BirdWhisperer.prototype.chirp = function () {
+        return 'Ah~ oh~ ' + this.chirping;
+    };
+    return BirdWhisperer;
+}());
+var birdWhisperer = new BirdWhisperer('coo-coo-coo...');
+document.body.innerHTML = birdWhisperer.chirp();
---
+++ jd/js/carousel_01.js
@@ -0,0 +1,74 @@
+// 获取图片
+// 获取按钮
+// 获取圆点
+
+let carouselItem=document.getElementsByClassName("carousel_item");
+let btnLeft=document.getElementsByClassName("left")[0];
+let btnRight=document.getElementsByClassName("right")[0];
+let pointsItem=document.getElementsByClassName("points_item");
+
+let index=0;//初始化一个索引值，用于切换图片。
+//两个按钮的切换效果开始 start
+let clearActive=function(){//提升图片显示层级后的清理器。
+    for(let i=0;i<carouselItem.length;i++){
+        carouselItem[i].className="carousel_item";
+        pointsItem[i].className="points_item";
+    }
+};
+
+let goRight=function(){//向右切换图片的函数
+    clearActive();//先清理
+    if(index===7){
+        index=0;
+    }
+    else{index++}
+    carouselItem[index].className="carousel_item active";
+    pointsItem[index].className="points_item active";
+}
+
+let goLeft=function(){//向左切换图片的函数
+    clearActive();//先清理
+    if(index===0){
+        index=7;
+    }
+    else{index--}
+    carouselItem[index].className="carousel_item active";
+    pointsItem[index].className="points_item active";
+}
+
+
+btnRight.addEventListener('click',goRight);
+btnLeft.addEventListener('click',goLeft);
+
+//两个按钮的切换效果结束 end
+
+//圆点的点击效果开始 start
+for(let i=0;i<pointsItem.length;i++){
+    pointsItem[i].addEventListener("click",function(){
+        index=pointsItem[i].getAttribute('data-index')-1;
+        goRight();
+    })
+}
+//圆点的点击效果结束 end
+//自动向右播放开始 start
+let goRightAuto=goRight;
+
+
+setInterval(function(){
+    goRightAuto();
+},3000)
+//自动向右播放结束 end
+
+let carousel_panel=document.getElementsByClassName("carousel_container block_mediate type_area")[0];
+
+carousel_panel.addEventListener("mouseenter",function(){
+    goRightAuto=function(){
+
+    }
+})
+carousel_panel.addEventListener("mouseleave",function(){
+    goRightAuto=goRight;
+})
+
+
+
