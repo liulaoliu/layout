266cba1652f723fb4b598d7f2820448011694bc3
liulaoliu <309503745@qq.com>
2019-03-13 11:25:51 +0800

    把扇形导航增加成了圆形导航 ，点击导航home会移动到屏幕中部

--- jd/js/sector_navigation.js
+++ jd/js/sector_navigation.js
@@ -1,14 +1,20 @@
+let locator=document.getElementsByClassName("sector_container")[0];
 let homeBtn=document.getElementsByClassName("sector_home")[0];
 let sector_item=document.getElementsByClassName("sector_item");
 
 //点击导航按钮，按钮旋转，再次点击按钮反转 开始
 //导航按钮旋转
 function rotate(){
+    locator.style.left="500px";
+    locator.style.bottom="500px";
     homeBtn.style.transform="rotate(360deg)";
     homeBtn.style.transition="1s";
 }
 function rotate_reverse(){
     homeBtn.style.transform="rotate(0deg)";
+    locator.style.left="0px";
+    locator.style.bottom="60px";
+    homeBtn.style.transition="1s";
 
 }
 let toggle=true;
@@ -27,7 +33,7 @@ homeBtn.addEventListener("click",function(){
 
 
 
-//点击导航按钮，扇形导航出现。
+//点击导航按钮，扇形导航出现。 开始
 
 //扇形导航位置移动
 function get_coordinate(length,deg){
@@ -41,8 +47,8 @@ function get_coordinate(length,deg){
 let len=260;
 function emerge(){
     for(let i=0;i<sector_item.length;i++){
-        sector_item[i].style.left=get_coordinate(len,22.5*i)["left"];
-        sector_item[i].style.bottom=get_coordinate(len,22.5*i)["bottom"];
+        sector_item[i].style.left=get_coordinate(len,36*i)["left"];
+        sector_item[i].style.bottom=get_coordinate(len,36*i)["bottom"];
         sector_item[i].style.transform="rotate(360deg)";
         sector_item[i].style.transition=1+i*0.2+"s";
 
@@ -57,4 +63,23 @@ function vanish(){
         sector_item[i].style.transform="rotate(0deg)";
         sector_item[i].style.transition=2-i*0.2+"s";
     }
+}
+//点击导航按钮，扇形导航出现。 结束
+
+
+
+//点击导航小按钮，出现放大的效果。
+function expand_clear(){
+    this.style.transition="0.1s";
+    this.style.transform="scale(1)";
+    this.removeEventListener("transitionend",expand_clear);
+}
+
+for(let i =0;i<sector_item.length;i++){
+    sector_item[i].addEventListener("click",function(){
+        this.style.transform="scale(2)";
+        this.style.transition="0.1s";
+        this.addEventListener("transitionend",expand_clear)
+    });
+
 }
